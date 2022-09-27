0a1cdd2b8c529fecd96082c74e1378e1127280ae
heyjin97 <byun970@gmail.com>
2022-09-27 10:20:15 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-const {
-    User
-} = require('./models/User');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
 
 const config = require('./config/key');
 
@@ -27,7 +26,7 @@ mongoose.connect(config.mongoURI)
 
 app.get('/', (req, res) => res.send('Hello World!~~'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원 가입 할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
@@ -45,7 +44,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({
@@ -78,13 +77,24 @@ app.post('/login', (req, res) => {
                         loginSuccess: true,
                         userId: user._id
                     })
-
-
-
             })
+        })
+    })
 
 
-
+    // role 1 어드민    role 2 특정 부서 어드민
+    // role 2 -> 일반유저   role 0 이 아니면 관리자
+    app.get('/api/users/auth', auth , (req, res) => {
+        //여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+        res.status(200).json({
+            _id: req.user._id,
+            isAdmin: req.user.role === 0 ? false: true,
+            isAuth: true,
+            email: req.user.email,
+            name: req.user.name,
+            lastname: req.user.lastname,
+            role: req.user.role,
+            image: req.user.image
         })
 
     })
